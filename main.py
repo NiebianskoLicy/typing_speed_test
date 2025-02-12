c5f976bf4d17118d924f9b953bbe6307eeb6784d
NiebianskoLicy <Robek0410@gmail.com>
2025-02-12 08:01:15 +0100

    finished

--- main.py
+++ main.py
@@ -8,38 +8,57 @@ window.geometry("720x450")
 font = ("Arial", 14)
 timer = 60
 
-# TODO 2: fix word count
-# TODO 3: finish after reaching to 0 seconds
-
+# getting text for program
 with open("words.json", "r") as f:
      text = json.load(f)["text"]
 
+# function which start countdown and show WPS
+def start_timer():
+    global timer, wps_text
+    #disable button for multi clicking it
+    start_button.config(state=DISABLED)
+    word_per_sec = round((word_count() / (timer/60)), 2)
 
-def start_timer(timer):
-    user_text.config(state=NORMAL)
-    countdown_text.after(1000,start_timer, timer-1)
-    timer -= 1
-    countdown_text.config(text=f"Countdown:\n{timer}")
-
-    word_per_sec = word_count()/(timer/60)
-    wps_text = Label(window, text=f"WPS:\n {word_per_sec}", font=("Arial", 14))
-    wps_text.after(1000, word_count, )
+    wps_text.config(text=f"WPS:\n {word_per_sec}", font=("Arial", 14))
+    wps_text.after(1000, word_count)
     wps_text.place(x=605, y=200)
 
+    user_text.config(state=NORMAL)
+    # start timer
+    st_tim = countdown_text.after(1000,start_timer,)
+    timer -= 1
 
+    countdown_text.config(text=f"Countdown:\n{timer}")
+
+    # on 0 sec program ends
+    if timer == 0:
+        user_text.config(state=DISABLED)
+
+        wps_text.config(text=f"Time's up!\n\nWPS: {word_per_sec}\nWPM: {round(word_count(), 2)}")
+        wps_text.place(
+            x=590,
+            y=190, )
+        countdown_text.after_cancel(st_tim)
+        # after reaching 0 time, user can restart again
+        timer = 60
+        start_button.config(text="Restart", command=start_timer, state=NORMAL)
+
+# a function which count words written
 def word_count():
-    word_per_sec = 0
+    count_word = 0
     count_text = user_text.get("1.0", END)
     word = ""
-    print(word_per_sec)
     for letter in count_text:
         word += letter
         if letter == " ":
             word = ""
             if word in text:
-                word_per_sec += 1
-                return word_per_sec
-    return word_per_sec
+                count_word += 1
+                return count_word
+    return count_word
+
+# outer variable allow to not overwrite labels
+wps_text = Label(window)
 
 
 #typping speed test
@@ -48,14 +67,17 @@ typ_sp_text.insert(END, text)
 typ_sp_text.place(x=10, y=10)
 typ_sp_text.config(state=DISABLED)
 
+# window for user text
 user_text = Text(window, height=8, width=50, font=font)
 user_text.place(x=10, y=220)
 user_text.config(state=DISABLED)
 
-start_button = Button(window, text="Start", command=lambda : start_timer(timer))
+# start button
+start_button = Button(window, text="Start", command=start_timer)
 start_button.config(font=("Arial", 14),width=5)
 start_button.place(x=580, y=361)
 
+# coundown label
 countdown_text = Label(
     window,
     text="Countdown:\n60",
